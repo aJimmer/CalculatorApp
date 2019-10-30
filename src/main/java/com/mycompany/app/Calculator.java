c1f76986e146a5610bbf6543a1c993836c8d23ae
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2019-10-29 18:12:32 -0700

    ..

--- src/main/java/com/mycompany/app/Calculator.java
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -2,7 +2,7 @@ package com.mycompany.app;
 
 public class Calculator
 {
-    private final String message = "Welcome to my Calculator";
+    private final String message = "Welcome to my Calculator App";
 
     public Calculator() {}
 
